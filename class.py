80738e8f38a48d2ba95b2679b425b4b6bc674630
mahmud10cool <mutashi@gmail.com>
2022-08-15 07:49:19 +0600

    Just another commit

--- class.py
+++ class.py
@@ -11,6 +11,19 @@ class Robot:
         # Self is an additional argument
         # This is for what he doesn't say yet
 
+class Person:
+    def __init__(self, n, p, i):
+        self.name = n
+        self.personality = p
+        self.is_sitting = i
+
+    def sit_down(self):
+        self.is_sitting = True
+
+    def stand_up(self):
+        self.is_sitting = False
+    
+
 # r1 = Robot()
 # r1.name = "Tom"
 # r1.color = "red"
@@ -26,3 +39,12 @@ r2 = Robot("Jerry", "blue", 40)
 
 r1.introduce_self()
 r2.introduce_self()
+
+p1 = Person("Alice", "aggressive", False)
+p2 = Person("Becky", "talkative", True)
+
+# p1 owns r2
+p1.robot_owned = r2
+p2.robot_owned = r1
+
+p1.robot_owned.introduce_self()
