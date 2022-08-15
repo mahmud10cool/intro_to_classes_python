fb3bd398e08e290e9b7a5a0c59c383f928d53ece
mahmud10cool <mutashi@gmail.com>
2022-08-15 08:21:46 +0600

    Ok. something new!

--- import.py
+++ import.py
@@ -7,6 +7,8 @@ import def_init as media
 # This will now throw and error because it is looking for arguments
 
 # New code
-toy_story = media.Movie("Toy Story", "A story of a boy", "https://en.wikipedia.org/wiki/File:Toy_Story.jpg", "https://www.youtube.com/watch?v=rNk1Wi8SvNc")
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come into life", 
+                        "https://en.wikipedia.org/wiki/File:Toy_Story.jpg", 
+                        "https://www.youtube.com/watch?v=rNk1Wi8SvNc")
 
 print(toy_story.storyline)
