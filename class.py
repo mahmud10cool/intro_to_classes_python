d10ef82d9c655745a6a744df1186587ce4465ca3
mahmud10cool <mutashi@gmail.com>
2022-08-15 07:37:10 +0600

    Empty commit

--- class.py
+++ class.py
@@ -5,21 +5,24 @@ class Robot:
         self.name = givenName
         self.color = givenColor
         self.weight = givenWeight
-        
+
     def introduce_self(self):
         print("My name is " + self.name)
         # Self is an additional argument
         # This is for what he doesn't say yet
 
-r1 = Robot()
-r1.name = "Tom"
-r1.color = "red"
-r1.weight = 30
+# r1 = Robot()
+# r1.name = "Tom"
+# r1.color = "red"
+# r1.weight = 30
 
-r2 = Robot()
-r2.name = "Jerry"
-r2.color = "blue"
-r2.weight = 40
+# r2 = Robot()
+# r2.name = "Jerry"
+# r2.color = "blue"
+# r2.weight = 40
+
+r1 = Robot("Tom", "red", 30)
+r2 = Robot("Jerry", "blue", 40)
 
 r1.introduce_self()
 r2.introduce_self()
