b312432917934f86b0f276c5370942009b76fbdb
mahmud10cool <mutashi@gmail.com>
2022-08-15 08:08:33 +0600

    something to do with name and main

---
+++ understand__name__.py
@@ -0,0 +1,12 @@
+# I should really prefer this
+# What is this name variable
+# dunder means double underscore
+
+
+def main():
+    print("Hello World")
+
+# Its to signal the reader that the code is script
+if __name__ == '__main__':
+    main()
+    _ = "don't use me" # unspoken rule not to use it
